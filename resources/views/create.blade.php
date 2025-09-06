be7d8f9552d78a8fad913b833783f0f75e1b63cd
Dex <a.markdexter@gmail.com>
2025-09-06 20:44:10 +0800

    use old alt helper

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -21,7 +21,7 @@
             <label for="title">
                 Title
             </label>
-            <input type="text" name="title" id="title" />
+            <input type="text" name="title" id="title" value="{{ old('title') }}" />
             @error('title')
                 <p class="error-message">{{ $message }}</p>
             @enderror
@@ -32,6 +32,7 @@
                 Description
             </label>
             <textarea name="description" id="description" rows="5">
+                {{ old('description') }}
             </textarea>
             @error('description')
                 <p class="error-message">{{ $message }}</p>
@@ -43,6 +44,7 @@
                 Long Description
             </label>
             <textarea name="long_description" id="long_description" rows="10">
+                      {{ old('long_description') }}
                 </textarea>
             @error('long_description')
                 <p class="error-message">{{ $message }}</p>
